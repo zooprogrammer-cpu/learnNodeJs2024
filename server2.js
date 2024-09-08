4752730fdbf7756c18696939225ca95cf65f9932
AshB <ashbasn@gmaill.com>
2024-09-07 22:58:32 -0500

    POST request

--- server2.js
+++ server2.js
@@ -39,6 +39,22 @@ const getUserByIdHandler = (req, res) => {
     res.end();
 }
 
+// Route handler for POST /api/users
+const createUserHandler = (req, res)=>{
+    let body = '';
+    // Listen for th data
+    req.on('data', (chunk)=>{
+        body += chunk.toString();
+    });
+    req.on('end', ()=>{
+        const newUser = JSON.parse(body);
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser));
+        res.end();
+    })
+}
+
 // Not found handler
 const notFoundHandler = (req, res) => {
     res.statusCode = 404;
@@ -56,6 +72,9 @@ const server = createServer((req, res) => {
                 getUsersHandler(req, res);
             } else if (req.url.match(/^\/api\/users\/\d+$/) && req.method === 'GET') {
                 getUserByIdHandler(req, res);
+            }
+            else if (req.url === '/api/users' && req.method === 'POST') {
+                createUserHandler(req, res);
             } else {
                 notFoundHandler(req, res);
             }
