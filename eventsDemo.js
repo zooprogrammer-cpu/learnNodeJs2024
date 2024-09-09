d1cf2f2b3e6b11659d903a1f4a4af1849481a135
AshB <ashbasn@gmaill.com>
2024-09-08 23:02:56 -0500

    eventsDemo

---
+++ eventsDemo.js
@@ -0,0 +1,28 @@
+import {EventEmitter} from 'events';
+
+const myEmitter = new EventEmitter();
+
+function greetHandler(name) {
+    console.log(`Hello ${name}`);
+}
+
+function goodbyeHandler(name) {
+    console.log(`Goodbye ${name}`);
+}
+
+// Register event listeners
+myEmitter.on('greet', greetHandler);
+myEmitter.on('goodbye', goodbyeHandler);
+
+// Emit events
+myEmitter.emit('greet', 'John');
+myEmitter.emit('goodbye', 'Sarah');
+
+//Error handling
+myEmitter.on('error', (err)=> {
+    console.log('An error occurred:', err);
+});
+
+// Simulate error
+myEmitter.emit('error', new Error('Something went wrong'));
+
